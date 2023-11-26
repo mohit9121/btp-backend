889a7b50f791b82bfd9afaa9f3e923491c244515
Mohit Vyas <vyasm912152@gmail.com>
2023-11-26 20:08:06 +0530

    requirements.txt updated

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from PIL import Image
+import numpy as np
+import io
+import keras as Keras
 
 app = Flask(__name__) 
+
+# model = Keras.models.load_model('./Prototype-Green_Hackathon_model.h5')
+
+# class_name = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
 
 @app.route('/')
 def hello_world():
@@ -9,6 +17,23 @@ def hello_world():
 @app.route('/test')
 def hello_world2():
     return 'Backend is running testing '
+
+# Define a route for disease prediction
+# @app.route('/predict', methods=['POST'])
+# def predict_disease():
+#     try:
+#         image = request.files['image']
+#         image = Image.open(io.BytesIO(image.read()))
+#         image = image.resize((100, 100))
+#         image = np.array(image)
+#         output = 'potato_unhealthy'
+#         prediction = model.predict(np.expand_dims(image, axis=0))
+#         prediction = prediction[0]
+#         print(class_name[np.argmax(prediction)])
+#         output = class_name[np.argmax(prediction)]
+#         return jsonify({'prediction': output})
+#     except Exception as e:
+#         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run()
