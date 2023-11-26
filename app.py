4fa3f2511378870d5436a2cbc750347132f91c04
Mohit Vyas <vyasm912152@gmail.com>
2023-11-26 20:23:34 +0530

    all parts uncommented

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import tensorflow as tf
 
 app = Flask(__name__) 
 
-# model = tf.keras.models.load_model('./Prototype-Green_Hackathon_model.h5')
+model = tf.keras.models.load_model('./Prototype-Green_Hackathon_model.h5')
 
-# class_name = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+class_name = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
 
 @app.route('/')
 def hello_world():
@@ -19,21 +19,21 @@ def hello_world2():
     return 'Backend is running testing '
 
 # Define a route for disease prediction
-# @app.route('/predict', methods=['POST'])
-# def predict_disease():
-#     try:
-#         image = request.files['image']
-#         image = Image.open(io.BytesIO(image.read()))
-#         image = image.resize((100, 100))
-#         image = np.array(image)
-#         output = 'potato_unhealthy'
-#         prediction = model.predict(np.expand_dims(image, axis=0))
-#         prediction = prediction[0]
-#         print(class_name[np.argmax(prediction)])
-#         output = class_name[np.argmax(prediction)]
-#         return jsonify({'prediction': output})
-#     except Exception as e:
-#         return jsonify({'error': str(e)})
+@app.route('/predict', methods=['POST'])
+def predict_disease():
+    try:
+        image = request.files['image']
+        image = Image.open(io.BytesIO(image.read()))
+        image = image.resize((100, 100))
+        image = np.array(image)
+        output = 'potato_unhealthy'
+        prediction = model.predict(np.expand_dims(image, axis=0))
+        prediction = prediction[0]
+        print(class_name[np.argmax(prediction)])
+        output = class_name[np.argmax(prediction)]
+        return jsonify({'prediction': output})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run()
