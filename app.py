5a3f8cb05f8678a1902aee83800eda348ea9a06a
Mohit Vyas <vyasm912152@gmail.com>
2024-03-21 12:40:15 +0530

    disease_name_format_corrected

--- app.py
+++ app.py
@@ -60,7 +60,48 @@ def predict():
         interpreter.invoke()
         output_data = interpreter.get_tensor(output_details[0]['index'])
  
-        plant_disease_names = ['Apple__Apple_scab', 'Apple_Black_rot', 'Apple_Cedar_apple_rust', 'Apple_healthy', 'Blueberry_healthy', 'Cherry(including_sour)__Powdery_mildew', 'Cherry(including_sour)__healthy', 'Corn(maize)__Cercospora_leaf_spot Gray_leaf_spot', 'Corn(maize)__Common_rust', 'Corn_(maize)__Northern_Leaf_Blight', 'Corn(maize)__healthy', 'Grape_Black_rot', 'Grape_Esca(Black_Measles)', 'Grape__Leaf_blight(Isariopsis_Leaf_Spot)', 'Grape__healthy', 'Orange_Haunglongbing(Citrus_greening)', 'Peach__Bacterial_spot', 'Peach_healthy', 'Pepper,_bell_Bacterial_spot', 'Pepper,_bell_healthy', 'Potato_Early_blight', 'Potato_Late_blight', 'Potato_healthy', 'Raspberry_healthy', 'Soybean_healthy', 'Squash_Powdery_mildew', 'Strawberry_Leaf_scorch', 'Strawberry_healthy', 'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites Two-spotted_spider_mite', 'Tomato_Target_Spot', 'Tomato_Tomato_Yellow_Leaf_Curl_Virus', 'Tomato_Tomato_mosaic_virus', 'Tomato__healthy']
+        # plant_disease_names = ['Apple__Apple_scab', 'Apple_Black_rot', 'Apple_Cedar_apple_rust', 'Apple_healthy', 'Blueberry_healthy', 'Cherry(including_sour)__Powdery_mildew', 'Cherry(including_sour)__healthy', 'Corn(maize)__Cercospora_leaf_spot Gray_leaf_spot', 'Corn(maize)__Common_rust', 'Corn_(maize)__Northern_Leaf_Blight', 'Corn(maize)__healthy', 'Grape_Black_rot', 'Grape_Esca(Black_Measles)', 'Grape__Leaf_blight(Isariopsis_Leaf_Spot)', 'Grape__healthy', 'Orange_Haunglongbing(Citrus_greening)', 'Peach__Bacterial_spot', 'Peach_healthy', 'Pepper,_bell_Bacterial_spot', 'Pepper,_bell_healthy', 'Potato_Early_blight', 'Potato_Late_blight', 'Potato_healthy', 'Raspberry_healthy', 'Soybean_healthy', 'Squash_Powdery_mildew', 'Strawberry_Leaf_scorch', 'Strawberry_healthy', 'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites Two-spotted_spider_mite', 'Tomato_Target_Spot', 'Tomato_Tomato_Yellow_Leaf_Curl_Virus', 'Tomato_Tomato_mosaic_virus', 'Tomato__healthy']
+
+        plant_disease_names = [
+    'Apple___Apple_scab', 
+    'Apple___Black_rot', 
+    'Apple___Cedar_apple_rust', 
+    'Apple___healthy', 
+    'Blueberry___healthy', 
+    'Cherry_(including_sour)___Powdery_mildew', 
+    'Cherry_(including_sour)___healthy', 
+    'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 
+    'Corn_(maize)___Common_rust_', 
+    'Corn_(maize)___Northern_Leaf_Blight', 
+    'Corn_(maize)___healthy', 
+    'Grape___Black_rot', 
+    'Grape___Esca_(Black_Measles)', 
+    'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 
+    'Grape___healthy', 
+    'Orange___Haunglongbing_(Citrus_greening)', 
+    'Peach___Bacterial_spot', 
+    'Peach___healthy', 
+    'Pepper,_bell___Bacterial_spot', 
+    'Pepper,_bell___healthy', 
+    'Potato___Early_blight', 
+    'Potato___Late_blight', 
+    'Potato___healthy', 
+    'Raspberry___healthy', 
+    'Soybean___healthy', 
+    'Squash___Powdery_mildew', 
+    'Strawberry___Leaf_scorch', 
+    'Strawberry___healthy', 
+    'Tomato___Bacterial_spot', 
+    'Tomato___Early_blight', 
+    'Tomato___Late_blight', 
+    'Tomato___Leaf_Mold', 
+    'Tomato___Septoria_leaf_spot', 
+    'Tomato___Spider_mites Two-spotted_spider_mite', 
+    'Tomato___Target_Spot', 
+    'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 
+    'Tomato___Tomato_mosaic_virus', 
+    'Tomato___healthy'
+]
 
         sorted_indices = np.argsort(output_data[0])[::-1]
         top_three_indices = sorted_indices[:5] 
